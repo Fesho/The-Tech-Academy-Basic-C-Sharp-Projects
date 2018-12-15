4db2cc12b9cda65f0d90f5aebde125efbf139652
Mostafa El-Fishawy <mostafa.prodev@gmail.com>
2018-12-15 15:08:50 +0200

    Create Program test1.cs

---
+++ Program test1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Welcome to Acme Accounting Sytems");
+            Console.WriteLine("Remember, We are ‘‘accounting’’ on you!");
+        }
+    }
+}
